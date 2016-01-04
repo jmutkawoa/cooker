18c526b3769c0ec07985005d9e271627b38257e9
root <root@localhost.localdomain>
2016-01-04 16:01:05 +0400

    Repository functionalities

--- build/lib/cooker.py
+++ build/lib/cooker.py
@@ -5,16 +5,86 @@
 
 from threading import Thread
 import os, re, tempfile, sys, time
-import fabric
+import fabric , fabric.operations, fabric.context_managers
 import ansible
+from fabric.api import *
 
-DEFAULT_PACKAGE = "apt"
-DEFAULT_ACTION	= "local"
-OPTIONS = dict(
-	package  = ["apt","yum"],
-	logging	 = ["y","n"],
-)
 
 class cooker:
-	def __init__(self,action = DEFAULT_ACTION ):
-		print action
+	DEFAULT_PACKAGE = "apt"
+	DEFAULT_ACTION	= "local"
+	SUDO			= False
+	PASS 			= False
+	KEY             = False
+	OPTIONS = dict(
+		package  = ["apt","yum"],
+		logging	 = ["y","n"],
+		action 	 = ["local", "remote"]
+	)
+	
+	def __init__(self,action = None ):
+		if not (action is None):
+			self.DEFAULT_ACTION = self.DEFAULT_ACTION
+			warn("%s Mode selected" % (action)) 
+		else:
+			assert action in self.OPTIONS['action'], "action must be one of: %s" % (self.OPTIONS['action'])
+		env.host_string = "127.0.0.1"
+	def isSudo(self):
+		return self.SUDO
+
+	def setMode(self,mode):
+		self.DEFAULT_ACTION = mode
+
+	def getMode(self):
+		return self.DEFAULT_ACTION
+
+	def connect(self,host=None,user=None,keyFile=None):
+		if (self.getMode() is "local"):
+			warn("You are in local mode not need to connect")
+		if (host is None):
+			sys.stderr.write("\nFatal error: %s\n" % "Host undefined")
+			sys.exit(1)
+		else:
+			env.host_string = host
+		if not (user is None):
+			self.user =  user
+			env.user = user
+		if not(keyFile is None):
+			env.key_filename = keyFile
+
+
+
+	def setPackage(self,package=None):
+		if not (package is None):
+			assert package in self.OPTIONS['package'], "Package must be one of: %s" % (self.OPTIONS['package'])
+			self.DEFAULT_PACKAGE = package
+
+	def getPackage(self):
+		return self.DEFAULT_PACKAGE
+
+	# ============================
+	#
+	# Repository functions
+	#=============================
+
+	def package_ensure(self,package):
+		def func_not_found(): 
+        		print "No Function " + self.i + " Found!"
+		func_name = self.DEFAULT_PACKAGE + "_package_ensure"
+		func = getattr(self,func_name,func_not_found)
+		func(package)
+
+	def apt_package_ensure(self,package):
+		if (self.DEFAULT_ACTION is "local"):
+			return local("apt-get install %s" % package)
+		if self.isSudo():
+			return sudo("apt-get install %s" % package)
+		return local("apt-get install %s" % package)
+
+	def yum_package_ensure(self,package):
+		if (self.DEFAULT_ACTION is "local"):
+			return local("yum install %s" % package)
+		if self.isSudo():
+			return sudo("yum install %s" % package)
+		return local("yum install %s" % package)
+
